f224b2f3bf3c1568fdd21a6e8d612873806f4ac1
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-08-24 01:48:42 +0545

    add some logic complete

--- server/server.js
+++ server/server.js
@@ -1,16 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import config from "./config/config.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = config.PORT;
 
 
 app.get("/", (req, res) => {
     res.send("API is running...");
 })
 
+// Connect to the database
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
