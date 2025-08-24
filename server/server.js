d83496ba1199988d9f171f3232f619a79627cd5e
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-08-24 22:11:21 +0545

    add and fix the server file

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import config from "./config/config.js";
 import connectDB from "./config/db.js";
 
+//routes
+import authRoutes from "./routes/auth.route.js";
+
 dotenv.config();
 
 const app = express();
@@ -12,9 +15,16 @@ const PORT = config.PORT;
 app.get("/", (req, res) => {
     res.send("API is running...");
 })
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Connect to the database
 connectDB();
+
+// routes
+app.use("/api/v1/auth", authRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
--- server/utils/hash.js
+++ server/utils/hash.js
@@ -3,8 +3,10 @@ import config from '../config/config.js';
 
 export const hashPassword = async (password) => {
     try {
-        const saltRound = config.SALT_ROUNDS;
-        const hashedPassword = await bcrypt.hash(password, saltRound);
+        const saltRounds = config.SALT_ROUNDS;
+        // const saltRounds = 13;
+
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
         return hashedPassword;
 
     } catch (err) {
@@ -18,6 +20,7 @@ export const comparePassword = async (password, hashedPassword) => {
     try {
         return await bcrypt.compare(password, hashedPassword);
     } catch (err) {
+        console.log(`Error occurred during password comparison: ${err.message}`);
         throw new Error('Internal server error');
     }
 }
